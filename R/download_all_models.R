b8b56c92aa7bdbb7a8df2accd7630f2310111b2a
frank hull <frankiethull@gmail.com>
2026-05-06 11:39:34 -0600

    update `download_all_models` default cache dir
    `cache_dir` arg defaults to `NULL` making the default cache based on the TabPFN python repo. recommendation by @edgararuiz :rocket:

--- R/download_all_models.R
+++ R/download_all_models.R
@@ -6,20 +6,25 @@
 #' This function routine will require you to sign a one-time license for both 2.5 & 2.6 model varieties. 
 #' Downloading all models will take some time, please be patient!
 #' 
-#' @param cache_dir a file path for the model cache 
+#' @param cache_dir an option to override the default cache directory
 #'
 #' @returns 
 #'
 #' @export
 #' @examples 
 #' \donttest{
-#' download_all_models(cache_dir = "~/.cache/tabpfn")
+#' download_all_models()
 #' }
-download_all_models <- function(cache_dir = "~/.cache/tabpfn"){
+download_all_models <- function(cache_dir = NULL){
 pathlib <- reticulate::import("pathlib")
 tabpfn <- reticulate::import("tabpfn")
-cache_dir <- pathlib$Path(path.expand(cache_dir))
+ model_loading <- tabpfn$model_loading
 
-  tabpfn$model_loading$download_all_models(to = cache_dir)
+  if(is.null(cache_dir)) {
+  cache_dir <- model_loading$get_cache_dir() # This is the key part, it extracts TabPFN's expected cache dir
+} else {
+    cache_dir <- pathlib$Path(path.expand(cache_dir))
+  }
+  model_loading$download_all_models(to = cache_dir)
 
 }
